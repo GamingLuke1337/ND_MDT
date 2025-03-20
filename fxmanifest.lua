6b1d33490865bdd35ccb3ed4fd8d4ea3d39de043
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-03-21 00:18:23 +0100

    Update fxmanifest.lua
    made it pretty

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,43 +1,44 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
-author "Andyyy7666"
-description "ND Framework MDT For LEO and FIRE"
-version "1.1.0"
+author 'Andyyy7666'
+description 'ND Framework MDT For LEO and FIRE'
+version '1.1.0'
+repository 'https://github.com/ND-Framework/ND_MDT'
 
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
 files {
-	"ui/index.html",
-	"ui/script.js",
-	"ui/style.css",
-    "ui/user.jpg",
-    "config/translate.json",
-    "config/charges.json",
-    "bridge/**/client.lua",
-    "modules/**/client.lua"
+    'ui/index.html',
+    'ui/script.js',
+    'ui/style.css',
+    'ui/user.jpg',
+    'config/translate.json',
+    'config/charges.json',
+    'bridge/**/client.lua',
+    'modules/**/client.lua'
 }
 
-ui_page "ui/index.html"
+ui_page 'ui/index.html'
 
 shared_scripts {
-    "@ox_lib/init.lua",
-    "config/config.lua",
-    "config/charges.json",
-    "bridge/**/shared.lua",
-    "source/shared.lua"
-}
-server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "source/server.lua"
-}
-client_scripts {
-    "source/client.lua"
+    '@ox_lib/init.lua',
+    'config/config.lua',
+    'config/charges.json',
+    'bridge/**/shared.lua',
+    'source/shared.lua'
 }
 
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'source/server.lua'
+}
+
+client_script 'source/client.lua'
+
 dependencies {
-    "oxmysql",
-    "ox_inventory",
-    "ox_lib"
+    'oxmysql',
+    'ox_inventory',
+    'ox_lib'
 }
